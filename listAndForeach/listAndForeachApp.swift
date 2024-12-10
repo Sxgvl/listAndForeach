74a39b77d184e5134db86da04755d09fd6f15b2b
Segal G <77511272+Sxgvl@users.noreply.github.com>
2024-12-10 03:59:00 +0100

    Initial Commit

---
+++ listAndForeach/listAndForeachApp.swift
@@ -0,0 +1,17 @@
+//
+//  listAndForeachApp.swift
+//  listAndForeach
+//
+//  Created by Segal GBENOU on 10/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct listAndForeachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
